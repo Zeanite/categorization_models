0b1a1d6d18d301d26fd29f8354bfe83d18c9b0a2
Zeanite <95304755+Zeanite@users.noreply.github.com>
2022-06-03 11:42:23 +0000

    update architecture

--- Algorithm/BSTree.h
+++ Algorithm/BSTree.h
@@ -3,3 +3,9 @@
 //  BSTree.h
 //  Algorithm
 //
+//  Created by Freya Hayashi on 1/20/17.
+//  Copyright (c) 2017 Freya Hayashi. All rights reserved.
+//
+
+#ifndef __Algorithm__BSTree__
+#define __Algorithm__BSTree__
